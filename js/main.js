60399418548a4b78264255a88a06d54353dbf557
Juana Rodriguez <rodriguezjuana212@gmail.com>
2025-09-17 17:16:26 -0500

    Cree la funcion que genera el nodo padre de la tarea con sus hijos usando js

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//Cree las constantes globales para los elementos principales 
+
+const input =document.getElementById("to-do-input");
+const addbtn = document.getElementById("add-btn");
+const contToDoList = document.getElementById("cont-to-do-list");
+const contCompletedList = document.getElementById("cont-completed");
+
+//Creamos la función que nos permite crear una nueva tarea 
+//Toda etiqueta que vamos a crear es apartir de la maqueta html pre-existente
+
+function createToDoItem(){
+    const item =document.createElement("div");
+} 
